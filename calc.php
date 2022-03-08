db6de12ed7d98558c10ae08ed2ec3b9723d9cf6a
olaleye <olaoduola01@gmail.com>
2022-03-08 08:34:56 +0000

    template of the calc

---
+++ calc.php
@@ -0,0 +1,19 @@
+<body>
+
+<form>
+<input type="text" name= "num1" placeholder="number 1">
+<input type="text" name= "num2" placeholder="number 2">
+<select name="operator" id="calc.php">
+    <option value="">Add</option>
+    <option value="">Subtract</option>
+    <option value="">Multiply</option>
+    <option value="">Divide</option>
+</select>
+<br>
+<button type="submit" name="submit">Calculate</button> 
+</form>
+
+
+
+
+</body>
