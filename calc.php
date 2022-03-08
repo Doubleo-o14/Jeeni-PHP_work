dcfb202e78740f0eac90c53b7d010a21e42dea1b
olaleye <olaoduola01@gmail.com>
2022-03-08 09:10:13 +0000

    all operators added

--- calc.php
+++ calc.php
@@ -3,16 +3,16 @@
 <form>
 <input type="text" name= "num1" placeholder="number 1">
 <input type="text" name= "num2" placeholder="number 2">
-<select name="operator" id="calc.php">
-    <option value="">Add</option>
-    <option value="">Subtract</option>
-    <option value="">Multiply</option>
-    <option value="">Divide</option>
+<select name="operator">
+    <option value="Add">Add</option>
+    <option value="Subtract">Subtract</option>
+    <option value="Multiply">Multiply</option>
+    <option value="Divide">Divide</option>
 </select>
 <br>
-<button type="submit" name="submit">Calculate</button> 
+<button type="submit" name="submit" value="submit">Calculate</button> 
 </form>
-
+<p>The answer is:</p>
 <?php
   if (isset($_GET['submit'])) {
     $results1 = $_GET['num1'];
@@ -21,6 +21,15 @@
     switch ($operator) {
       case Add:
         echo $results1 + $results2;
+        break;
+      case Subtract:
+        echo $results1 - $results2;
+        break;
+      case Divide:
+        echo $results1 / $results2;
+        break;
+      case Multiply:
+        echo $results1 * $results2;
         break;
 
     }
